7a5f677b46a56d08d00f51f3dced42a6cbb4bf50
Fdamirez <fabianramirez0072@gmail.com>
2022-12-04 12:38:00 -0500

    feat read excel validtions and databank

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,36 +1,38 @@
 import string
 import openpyxl
 
-path = 'files/'
+pathInit = 'files/'
 
+def readfiles(path, val):
+    ABCD = list(string.ascii_uppercase)  # matriz con todas las letras del abcedario
+    wb = openpyxl.load_workbook(pathInit+path)
 
-
-ABCD = list(string.ascii_uppercase)  # matriz con todas las letras del abcedario
-wb = openpyxl.load_workbook(path+"Validations.xlsx")
-
-sheet = wb.active
-max_col = sheet.max_column
-
-
-#wb[a]
-
-print(wb["A"])
-
-excel_data = []
-
-for letter in ABCD:
-    print(letter)
-    data_letter = []
-    sheet = wb[letter]
+    sheet = wb.active
     max_col = sheet.max_column
-    for i in range(25) :
-        row = []
-        for i in range(1, max_col + 1):
-            cell_obj = sheet.cell(row=1, column=i)
-            row.append(cell_obj.value)
-        data_letter.append(row)
-    excel_data.append(data_letter)
-print(excel_data)
+    # wb[a]
+    print(wb["A"])
+    excel_data = []
 
-            # Escojo la primera por su nombre
-# La variable es un objeto <sheet>
+    for letter in ABCD:
+        sheet = wb[letter]
+        max_col = sheet.max_column
+        print("Voy por la ", letter)
+        for i in range(25):
+            row = []
+            for j in range(1, max_col + 1):
+                cont = i+1
+                if (val == 0):
+                    cont = 1
+                cell_obj = sheet.cell(row=cont, column=j)
+                row.append(cell_obj.value)
+            excel_data.append(row)
+    return excel_data
+
+
+DataBank= readfiles("DataBank.xlsx", 1)
+
+print("DataBank ", DataBank)
+
+validations = readfiles("Validations.xlsx", 0)
+
+print("Validaciones ", validations)
