f09b25b67d2d6011f0433723effba145809fedf9
camilobrq <79210792+camilobrq@users.noreply.github.com>
2022-12-03 22:10:18 -0500

    diseño

--- Reconocimiento.py
+++ Reconocimiento.py
@@ -1,24 +1,23 @@
 import tkinter as tk
 from tkinter import *
-from PIL import Image, ImageTk
+#from PIL import Image, ImageTk
 from tkinter import messagebox, Label, Entry, StringVar
-
 
 print("Librerias Leidas")
 
-def loadImage():
-    img = Image.open("Test/PC.jpg")
-    new_img = img.resize((300,256))
-    render = ImageTk.PhotoImage(new_img)
-    imgView = Label(windows, image=render)
-    imgView.image = render
-    imgView.place(x=350, y=150)
-    labelResult = Label(windows, text="El reconocimiento Determino la letra (A)")
-    labelResult.place(x=350, y=410)
+#def loadImage():
+ #   img = Image.open("Test/PC.jpg")
+ #  new_img = img.resize((300,256))
+ #   render = ImageTk.PhotoImage(new_img)
+ #   imgView = Label(windows, image=render)
+ #   imgView.image = render
+ #   imgView.place(x=250, y=220)
+ #   labelResult = Label(windows, text="El reconocimiento Determino la letra (A)")
+ #   labelResult.place(x=250, y=410)
 
 def loadMessage():
-    labelframe = Label(windows, text="No sea Bruto Eso todavia no sirve")
-    labelframe.place(x=350, y=410)
+    labelframe = Label(windows, text="No sea Bruto Eso todavia no sirve", bg="#88cffa")
+    labelframe.place(x=250, y=410)
 
     print(Theta.get())
     print(Error.get())
@@ -27,38 +26,39 @@ def loadMessage():
 def loadCompents(windows):
 
 
-
-    labelframe = Label(windows, text="Entrenamiento de redes neuronales con Bagpropagations")
+    windows.title("Redes Neuronales Bagpropagations")
+    #image_fondo=tk.PhotoImage(file="1.png")
+    windows.configure(bg="#88cffa")
+    labelframe = Label(windows, text="Entrenamiento de redes neuronales con Bagpropagations", font=("Arial",18,"bold"),bg="#88cffa")
     labelframe.place(x=10, y=30)
 
-    labelSubtitle = Label(windows, text="Ingrese los parametrros de entramiento de la red")
-    labelSubtitle.place(x=10, y=80)
+    labelSubtitle = Label(windows, text="Parametrros de entramiento de la red", font=("Arial",15), bg="#88cffa")
+    labelSubtitle.place(x=140, y=80)
 
 
-    labelEpoch = Label(windows, text="Iteraciones")
-    labelEpoch.place(x=10, y=150)
+    labelEpoch = Label(windows, text="Iteraciones", font=("Arial",13), bg="#88cffa")
+    labelEpoch.place(x=10, y=130)
 
     entryEpoch = Entry(windows, relief="flat", width=20, textvariable=epoch)
-    entryEpoch.place(x=10, y=180)
+    entryEpoch.place(x=10, y=160)
 
-    labelError = Label(windows, text="Error Permitido")
-    labelError.place(x=10, y=230)
+    labelError = Label(windows, text="Error Permitido",font=("Arial",13), bg="#88cffa")
+    labelError.place(x=170, y=130)
 
     entryError = Entry(windows, relief="flat", width=20, textvariable=Error)
-    entryError.place(x=10, y=260)
+    entryError.place(x=170, y=160)
 
-    labelTheta = Label(windows, text="Taza de aprenzaje")
-    labelTheta.place(x=10, y=310)
+    labelTheta = Label(windows, text="Taza de aprenzaje", font=("Arial",13), bg="#88cffa")
+    labelTheta.place(x=320, y=130)
 
     entryTheta = Entry(windows, relief="flat", width=20, textvariable=Theta)
-    entryTheta.place(x=10, y=340)
+    entryTheta.place(x=324, y=160)
 
+    #buttonLoad = tk.Button(windows, command=loadImage, text="Cargar Imagen", height=2, width=15, borderwidth=5)
+    #buttonLoad.place(x=280, y=450)
 
-    buttonLoad = tk.Button(windows, command=loadImage, text="Cargar Imagen", height=2, width=15)
-    buttonLoad.place(x=300, y=450)
-
-    buttonLearn = tk.Button(windows, command=loadMessage, text="Entrenar La red", height=1, width=15)
-    buttonLearn.place(x=10, y=400)
+    buttonLearn = tk.Button(windows, command=loadMessage, text="Entrenar La red", height=1, width=15, border=5)
+    buttonLearn.place(x=520, y=145)
 
 
 
